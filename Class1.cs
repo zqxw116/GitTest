146f64f3797fb2af486602724897d37eb0a4cd76
zqxw116 <zqxw116@naver.com>
2021-11-23 16:07:50 +0900

    git study test

---
+++ Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace SUM
+{
+    class Program
+    {
+        public static int Sum(params int[] nums)
+        {
+            int sum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+            }
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arry = { 3, 4, 5 };
+            Debug.Writeline(Sum(arry));
+            Debug.Writeline(Sum(4, 7, 8, 10, 11));
+            Debug.Writeline(Sum(new int[5] { 1, 2, 3, 4, 5 }));
+        }
+    }
+}
